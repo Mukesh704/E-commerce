605a41c9d2b938fbf213b8843b91fd64b5bbaa14
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-05-24 13:49:14 +0530

    authorization middleware created

--- BACKEND/models/userModel.js
+++ BACKEND/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
   {
@@ -24,5 +25,28 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if(!user.isModified('password')) next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(user.password, salt);
+    user.password = hashedPass;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+})
+
+userSchema.methods.comparePassword = async function(userPassword) {
+  try {
+    const isMatch = await bcrypt.compare(userPassword, this.password);
+    return isMatch;
+  } catch (err) {
+    throw err;
+  }
+}
 
 module.exports = mongoose.model('User', userSchema);
