e97ef44529d41057aae6fca3c7ff2c99db401564
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-05-28 17:08:23 +0530

    admin update controller completed

--- BACKEND/routes/adminOrderRouter.js
+++ BACKEND/routes/adminOrderRouter.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { getAllOrdersController, updateOrderStatusController } = require('../controllers/adminOrderController');
+const { jwtAdminMiddleware } = require('../middlewares/adminMiddleware')
 
-router.get('/', getAllOrdersController);
-router.put('/:id', updateOrderStatusController);
+router.get('/', jwtAdminMiddleware, getAllOrdersController);
+router.put('/:id', jwtAdminMiddleware, updateOrderStatusController);
 
 module.exports = router;
