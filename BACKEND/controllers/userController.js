7ca994c76ca63e5b27c2ebb4fac198f1ff49918c
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-05-28 16:37:06 +0530

    completed remaining user controllers

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/userModel')
+const orderModel = require('../models/orderModel')
 
 async function getUserProfileController(req, res) {
     try {
@@ -45,9 +46,56 @@ async function updateUserProfileController(req, res) {
     }
 };
 
-function getOrdersController(req, res) {};
+async function getOrdersController(req, res) {
+    try {
+        const userId = req.user.id;
 
-function getWishListController(req, res) {};
+        const orders = await orderModel.find({user: userId}).populate('orderItems.product')
+
+        if(!orders || orders.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: 'No orders found for the user'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            orders
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: 'Internal server error'
+        })
+    }
+};
+
+async function getWishListController(req, res) {
+    try {
+        const userId = req.user.id;
+        const user = await userModel.findById(userId).populate('wishlist');
+
+        if(!user || !user.wishlist || user.wishlist.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: 'No items in the wishlist'
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            wishlist: user.wishlist
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: 'Internal server error'
+        })
+    }
+};
 
 module.exports = {
     getUserProfileController,
